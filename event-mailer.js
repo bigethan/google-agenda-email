5d915bfd773261b2e0b9496c98cc31938dfe503a
Big Ethan <e@bigethan.com>
2018-04-09 14:40:53 -0700

    Adding free time calculator
    Found that knowing how much free time I have for the next day has been helpful.  Added the ability to recognize "fake" meetings by appending a 🛡 to the meeting name (meetings you book to ensure you actually have free time)

--- event-mailer.js
+++ event-mailer.js
@@ -6,8 +6,17 @@
  *
  * Why? I use this to remind me via personal email what I'm
  * going to be doing the next day at work without having to
- * load my whole work google account onto my phone.
+ * scan my work email / calendar
  */
+
+function getMinutesBetweenDates(startDate, endDate) {
+    var diff = endDate.getTime() - startDate.getTime();
+    return (diff / 60000);
+}
+
+function willAttendMeeting(status) {
+  return (status === CalendarApp.GuestStatus.OWNER || status === CalendarApp.GuestStatus.YES || status === CalendarApp.GuestStatus.MAYBE);
+}
 
 function getEventsAndMail() {
   // message defaults - set to address in the script properties
@@ -31,6 +40,9 @@ function getEventsAndMail() {
   var events;
   var eventTime;
   var eventTitle;
+  var eventStartDate;
+  var eventEndDate;
+  var dayFreeMin = 8 * 60; //an eight hour day
 
   // Get Date objects for tomorrow morning and night
   var d = new Date();
@@ -51,20 +63,34 @@ function getEventsAndMail() {
     } else {
       // create the list
       for (var i = 0; i < events.length; i++) {
-        startSplit = events[i].getStartTime().toTimeString().split(':');
-        niceStart = startSplit[0] + ':' + startSplit[1]
-        endSplit = events[i].getEndTime().toTimeString().split(':');
-        niceEnd = endSplit[0] + ':' + endSplit[1];
-        eventLoc = events[i].getLocation();
-        niceLoc = '';
-        if (eventLoc) {
-          locSplit = eventLoc.split('(');
-          niceLoc = ' in ' + locSplit[0].trim()
+        if (willAttendMeeting(events[i].getMyStatus())) { //otherwise shows _everything_
+          eventStartDate = events[i].getStartTime();
+          eventEndDate = events[i].getEndTime();
+          eventTitle = events[i].getTitle().trim();
+          
+          //logic to ignore time blocked shield meetings indicated by a trailing sheild emoji 🛡
+          // TODO: Notice when you're double booked, currently subtracts everything
+          if (!/🛡$/.test(eventTitle)) {
+            dayFreeMin = dayFreeMin - getMinutesBetweenDates(eventStartDate, eventEndDate);
+          }
+          
+          startSplit = eventStartDate.toTimeString().split(':');
+          niceStart = startSplit[0] + ':' + startSplit[1]
+          endSplit = eventEndDate.toTimeString().split(':');
+          niceEnd = endSplit[0] + ':' + endSplit[1];
+          eventLoc = events[i].getLocation();
+          niceLoc = '';
+          if (eventLoc) {
+            locSplit = eventLoc.split('(');
+            niceLoc = ' in ' + locSplit[0].trim();
+          }
+          message += niceStart + ' - ' + niceEnd + niceLoc + "\n\t"
+          + eventTitle + "\n";
         }
-        message += niceStart + ' - ' + niceEnd + niceLoc + "\n\t"
-          + events[i].getTitle().trim() + "\n";
       }
     }
+    
+    message = "You have " + Math.floor(dayFreeMin/60) + " free hours tomorrow \n\n" + message;
 
     MailApp.sendEmail(mailTo, subject, message);
   }
